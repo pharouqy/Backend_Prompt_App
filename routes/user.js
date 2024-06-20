d9a820d48ea1a66489abb4a5c74f04d8be1b68c8
pharouqy <pharouky@gmail.com>
2024-06-20 16:39:22 +0100

    Fixed up routes !

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,8 @@ const auth = require("../midellwares/auth");
 router.post("/login", limiter, contrlrUser.login);
 router.post("/register", limiter, contrlrUser.register);
 router.get("/logout", auth, contrlrUser.logout);
+router.get("/test", (res, next) => {
+    res.status(200).json({ message: "test" });
+});
 
 module.exports = router;
